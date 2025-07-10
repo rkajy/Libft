79eeb70876024d57b907dbd26cddb4f497b6e3ce
Rado Andriamahakajy <radandri@1-E-8.42heilbronn.de>
2025-07-10 17:24:28 +0200

    try to fix error 2 to make ci cd pass

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -6,11 +6,11 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:16:18 by radandri          #+#    #+#             */
-/*   Updated: 2025/07/09 00:16:47 by radandri         ###   ########.fr       */
+/*   Updated: 2025/07/10 17:23:19 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {

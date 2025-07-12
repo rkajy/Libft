483f090a6311904f7ec0207ddc7a3fc41dfa1e3d
Rado Andriamahakajy <radandri@1-B-9.42heilbronn.de>
2025-07-12 12:59:43 +0200

    remove restrict keyword

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,12 +6,12 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:09:39 by radandri          #+#    #+#             */
-/*   Updated: 2025/07/10 19:13:07 by radandri         ###   ########.fr       */
+/*   Updated: 2025/07/12 12:57:24 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 }
--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 22:55:01 by radandri          #+#    #+#             */
-/*   Updated: 2025/07/11 11:12:12 by radandri         ###   ########.fr       */
+/*   Updated: 2025/07/12 12:57:27 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
+size_t	ft_strlcpy(char *dst, const char *src,
 			size_t dstsize);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src,
+size_t	ft_strlcat(char *dst, const char *src,
 			size_t dstsize);
 int		ft_tolower(int c);
 int		ft_toupper(int c);

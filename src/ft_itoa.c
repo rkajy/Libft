bcbffb5baae24e2b2e7b9a6df83a7ce19917a928
Rado Andriamahakajy <radandri@1-C-11.42heilbronn.de>
2025-07-18 14:03:52 +0200

    itoa done

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,16 +6,16 @@
 /*   By: radandri <radandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 22:22:23 by radandri          #+#    #+#             */
-/*   Updated: 2025/07/18 02:19:54 by radandri         ###   ########.fr       */
+/*   Updated: 2025/07/18 14:02:52 by radandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_int_len(int n)
+static int	ft_int_len(long n)
 {
-	int isneg;
-	int len;
+	int	isneg;
+	int	len;
 
 	isneg = 0;
 	len = 0;
@@ -25,43 +25,40 @@ static int ft_int_len(int n)
 		n = n * -1;
 		len++;
 	}
-	if (n == 0 )
+	if (n == 0)
 		len++;
 	while (n > 0)
 	{
-		n = n/10;
+		n = n / 10;
 		len++;
 	}
-	return len;
+	return (len);
 }
-#include <stdio.h>
+
 char	*ft_itoa(int n)
 {
-	int n_len;
-	char *res;
-	int	isneg;
+	int		n_len;
+	char	*res;
+	int		isneg;
+	int		temp;
+	long	nbr;
 
-	if (n == -2147483648)
-		return "-2147483648";
-	
-	n_len = ft_int_len(n);
-	isneg = 0;
-	if (n < 0)
-	{		
-		isneg = 1;
-		n = n * -1;
-	}
-	res = malloc((n_len + 1)*sizeof(char));
+	nbr = n;
+	n_len = ft_int_len(nbr);
+	isneg = n < 0;
+	if (isneg)
+		nbr = -nbr;
+	res = malloc((n_len + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	res[n_len] = '\0';
 	while (n_len != 0)
 	{
-		res[n_len-1] = (n % 10) + '0';
-		n = n / 10;
-		n_len--;
+		temp = (n_len--) - 1;
+		res[temp] = (nbr % 10) + '0';
+		nbr = nbr / 10;
 	}
 	if (isneg)
 		res[n_len] = '-';
-	return res;
+	return (res);
 }
